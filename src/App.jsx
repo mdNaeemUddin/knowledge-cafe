059a6228317c6fae08d32fe46d41697eeab477ce
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-03-15 14:40:46 +0600

    module 42 practice

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     
+      <p className="text-4xl bg-red-500 p-8">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
